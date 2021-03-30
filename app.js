4d29c1448a7018353c3b0e6ad73ca552c765fe92
Cole <colejcrutcher@gmail.com>
2021-03-30 13:49:26 -0500

    Index page loads with dynamic data from book list. Can see all books that the library carries.

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://**UserData**@cluster0.7uis0.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = 'mongodb+srv://@cluster0.7uis0.mongodb.net/local_library?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
