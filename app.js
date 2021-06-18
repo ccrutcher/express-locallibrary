eb90570e9de4fe481a25139f53d97cdedcbc8ced
Cole <colejcrutcher@gmail.com>
2021-06-18 14:18:00 -0500

    Environment variable fix

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = process.env.mongoDB;
+var mongoDB = process.env.MONGODB;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
